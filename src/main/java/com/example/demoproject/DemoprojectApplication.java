f125a938ffa9845b64c8d86ab0c0e5b9fa8f82c7
chadipiralla2428 <schadipiralla@gmail.com>
2023-03-21 15:11:14 +0530

    second demo commit

--- src/main/java/com/example/demoproject/DemoprojectApplication.java
+++ src/main/java/com/example/demoproject/DemoprojectApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoprojectApplication {
+	
 
 	public static void main(String[] args) {
+		System.out.println("Hello jenkins demo");
 		SpringApplication.run(DemoprojectApplication.class, args);
 	}
 
