1743230cfa6935f07d6c3a36343c9cebae803982
chadipiralla2428 <schadipiralla@gmail.com>
2023-03-21 20:00:33 +0530

    latest commit

--- src/main/java/com/example/demoproject/DemoprojectApplication.java
+++ src/main/java/com/example/demoproject/DemoprojectApplication.java
@@ -9,6 +9,7 @@ public class DemoprojectApplication {
 
 	public static void main(String[] args) {
 		System.out.println(10/0);
+		System.out.println("welocme jenkins");
 		SpringApplication.run(DemoprojectApplication.class, args);
 	}
 
